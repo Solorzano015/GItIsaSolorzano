1b73804d65e324fc265f1901db79f489690ae384
BERRY\casti <castillootamendimaria@gmail.com>
2025-02-03 17:10:21 +0100

    Rama 4: Cambio de nivel. (Script y layout)

---
+++ Assets/Scrips/ChangeLevel.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+
+    }
+}
