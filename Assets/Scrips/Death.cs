0d252765950453e211ee71fa3b5e83728c4ece41
BERRY\casti <castillootamendimaria@gmail.com>
2025-02-03 16:44:58 +0100

    Segunda rama: Script "Death"

---
+++ Assets/Scrips/Death.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Death : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+}
